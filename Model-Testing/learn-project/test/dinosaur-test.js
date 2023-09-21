0d29c710ff3e293ec24933ab26238623efeb174c
Alexei Rusu <rusualexei1995@gmail.com>
2023-09-20 20:59:08 -0400

    Model Testing

---
+++ Model-Testing/learn-project/test/dinosaur-test.js
@@ -0,0 +1,13 @@
+const Dinosaur = require('../dinosaur');
+const {assert} = require('chai');
+const {mongoose, databaseUrl, options} = require('../../database');
+
+describe('Dinosaur', () => {
+
+  describe('#name', () => {
+    it('is a String', () => {
+      const dino = new Dinosaur()
+
+    });
+  });
+});
