9a66f8e8ad97da11f15dd856b1dbc683e58bea45
Alexei Rusu <rusualexei1995@gmail.com>
2023-09-22 13:08:53 -0400

    Developed a model using TDD

--- Model-Testing/learn-project/dinosaur.js
+++ Model-Testing/learn-project/dinosaur.js
@@ -1,10 +1,30 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// Define schema here
-const DinosaurSchema = new Schema({
-  name: String
-})
+const validator = function(value) {
+  return value <= 10;
+};
+
+const DinosaurSchema = new Schema(
+  {
+    name: {type: String, required: true},
+    count: {
+      type: Number,
+      max: [10, 'Cannot hold more than 10 dinosaurs.']
+    },
+    risk: {type: String}
+  }
+);
+
+// method .findByName
+DinosaurSchema.statics.findByName = function(name, callback) {
+  return this.findOne({ name: name }, callback);
+};
+
+// method .breed
+DinosaurSchema.methods.breed = function(arg){
+  this.count =this.count + 1
+}
 
 
 module.exports = mongoose.model('Dinosaur', DinosaurSchema);
--- Model-Testing/learn-project/test/dinosaur-test.js
+++ Model-Testing/learn-project/test/dinosaur-test.js
@@ -3,10 +3,66 @@ const {assert} = require('chai');
 const {connectAndDrop, disconnect} = require('../database');
 
 describe('Dinosaur', () => {
-  // Add hooks here
   beforeEach(connectAndDrop);
-
   afterEach(disconnect);
+
+  describe('#name', () => {
+    it('is a String', () => {
+      const dino = new Dinosaur({
+        name: 'T-rex'
+      });
+
+      assert.strictEqual(dino.name, 'T-rex');
+    });
+
+    it('is required', () => {
+      const dino = new Dinosaur({});
+
+      const error = dino.validateSync();
+
+      assert.equal(error.errors.name.message, 'Path `name` is required.');
+      assert.equal(error.errors.name.kind, 'required');
+    });
+  });
+
+  describe('#count', () => {
+    it('is below 11 if risk is high', () => {
+      const dino = new Dinosaur({
+        name: 'T-rex',
+        count: 11,
+        risk: 'High'
+      });
+
+      dino.validateSync();
+
+      assert.ok(dino.errors, 'model should be invalid');
+      assert.equal(dino.errors.count.message, 'Cannot hold more than 10 dinosaurs.');
+    });
+
+    it('is valid with 10', () => {
+      const dino = new Dinosaur({
+        name: 'Triceratops',
+        count: 10,
+        risk: 'Low'
+      });
+
+      dino.validateSync();
+
+      assert.isUndefined(dino.errors, 'model should be valid');
+    });
+  });
+
+  describe('#risk', () => {
+    it('is a String', () => {
+      const dino = new Dinosaur({
+        name: 'T-rex',
+        risk: 'High'
+      });
+
+      assert.strictEqual(dino.risk, 'High');
+    });
+  });
+
 
   describe('#save', () => {
     it('persists a dino', async () => {
@@ -18,20 +74,49 @@ describe('Dinosaur', () => {
       const dino = new Dinosaur(fields);
 
       await dino.save();
-      const stored = await Dinosaur.find({ 'name': 'Velociraptor' });
+      const stored = await Dinosaur.findOne({ 'name': 'Velociraptor' });
 
-      assert.strictEqual(stored.length, 1);
-      assert.include(stored[0], fields);
+      assert.include(stored, fields);
     });
   });
 
-  describe('#name', () => {
-    it('is a String', () => {
-      const dino = new Dinosaur({
-        name: 'T-rex',
-      });
+  describe('.findByName', () => {
+    it('returns the first match on name', async () => {
+      const fields = {
+        name: 'Pterodactyl',
+        count: 9,
+        risk: 'Low'
+      };
+      const dino = new Dinosaur(fields);
+      await dino.save();
 
-      assert.strictEqual(dino.name, 'T-rex');
+      const stored = await Dinosaur.findByName('Pterodactyl');
+
+      assert.include(stored, fields);
+    });
+  });
+
+  describe('#breed', () => {
+    it('increases count by 1',  () => {
+      const start = 3;
+      const end = 4;
+      const dino = new Dinosaur({
+        name: 'Stegosaurus',
+        count: start,
+        risk: 'Low'
+      });
+      dino.breed()
+      assert.strictEqual(dino.count, end)
     });
   });
 });
+
+
+
+// ---- - Review
+// Developed a model using TDD!
+
+// The model layer represents entities and interactions in a web app’s problem domain.
+// Model paths can be test-driven using validators. Call validateSync and make assertions on the properties of [instance].errors.[path].
+// The storage of data can be tested with construction and updating methods like save and update. Retrieval can be tested with query methods like find, findOne, and findby.
+// Static methods are stored in [schema].statics and instance methods are stored in [schema].methods. Both can be tested like any other JavaScript function.
